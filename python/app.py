73ef3fc0199f9664eed75fa2e343a35221223fec
Ravi Eragapati <e.ravikanth@live.com>
2026-01-30 20:42:34 -0500

    Implement cold start logging in handler
    Add cold start logging to the handler function

--- python/app.py
+++ python/app.py
@@ -1,1 +1,18 @@
 
+import time
+
+cold_start = True
+init_time = time.time()
+
+def handler(event, context):
+    global cold_start
+
+    start = time.time()
+
+    print("Language=Python")
+    print(f"ColdStart={cold_start}")
+    print(f"InitDurationMs={(time.time() - init_time) * 1000:.2f}")
+
+    cold_start = False
+
+    print(f"ExecutionDurationMs={(time.time() - start) * 1000:.2f}")
