6aaee720c92538b8b3ad2f318e05cc244cb94a3d
Ravi Eragapati <e.ravikanth@live.com>
2026-01-30 20:41:58 -0500

    Add AWS Lambda handler with cold start metrics
    Implement AWS Lambda handler in Go with cold start logging.

--- go/main.go
+++ go/main.go
@@ -1,1 +1,30 @@
 
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var coldStart = true
+var initTime = time.Now()
+
+func handler(ctx context.Context) error {
+	start := time.Now()
+
+	fmt.Printf("Language=Go\n")
+	fmt.Printf("ColdStart=%v\n", coldStart)
+	fmt.Printf("InitDurationMs=%d\n", time.Since(initTime).Milliseconds())
+
+	coldStart = false
+
+	fmt.Printf("ExecutionDurationMs=%d\n", time.Since(start).Milliseconds())
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
