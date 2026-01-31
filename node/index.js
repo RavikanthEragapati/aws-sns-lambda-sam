f3c7053896178fa66b894a039ce49246d3aa740b
Ravi Eragapati <e.ravikanth@live.com>
2026-01-30 20:42:18 -0500

    Implement cold start logging in handler
    Add cold start logging to the handler function

--- node/index.js
+++ node/index.js
@@ -1,1 +1,15 @@
 
+let coldStart = true;
+const initTime = Date.now();
+
+exports.handler = async () => {
+  const start = Date.now();
+
+  console.log("Language=Node.js");
+  console.log(`ColdStart=${coldStart}`);
+  console.log(`InitDurationMs=${Date.now() - initTime}`);
+
+  coldStart = false;
+
+  console.log(`ExecutionDurationMs=${Date.now() - start}`);
+};
